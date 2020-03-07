32c9af21bab0a65af66ddd534fd2f78b5eea6fe1
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-03-06 23:01:05 -0400

    Configuración Logica admin

--- MicroProyecto2/src/app/components/admin/admin.component.ts
+++ MicroProyecto2/src/app/components/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../../services/firestore.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  reservas: any[];
 
+  constructor(private firestore: FirestoreService) {
+    this.firestore.getAll().subscribe((comprasSnapshot) => {
+      this.reservas = [];
+      comprasSnapshot.forEach((compraData: any) => {
+        this.reservas.push({
+          id: compraData.payload.doc.id,
+          data: compraData.payload.doc.data()
+        });
+      });
+    });
+  }
   ngOnInit(): void {
   }
 
